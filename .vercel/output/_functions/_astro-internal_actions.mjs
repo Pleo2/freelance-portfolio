9b52bd70253a7a3b18caa3e8edd1cd032aa9d611
pleo2 <leooel23m@gmail.com>
2025-03-23 23:49:11 -0400

    chore: remove unused Vercel output files and update tsconfig to exclude additional directories

--- .vercel/output/_functions/_astro-internal_actions.mjs
+++
@@ -1,1 +0,0 @@
-export { s as server } from './chunks/_astro-internal_actions_DST-_n0S.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_BFOxCx45.mjs';
-export { renderers } from '../renderers.mjs';
